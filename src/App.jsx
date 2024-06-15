1dbd2fcc2feb0251c344b2319943a8d54777e93f
akshayskumar004 <akshayskumar004@gmail.com>
2024-06-16 03:04:25 +0530

    Route implemenation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
-// eslint-disable-next-line no-unused-vars
-import React from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signin from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
-  return <div className="text-red-600">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
