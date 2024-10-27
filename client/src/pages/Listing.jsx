66b5b63d680d47c4c5b596cb17c91696cc2f9476
akshayskumar004 <akshayskumar004@gmail.com>
2024-10-27 18:26:37 +0530

    completed  the project

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -92,7 +92,7 @@ export default function Listing() {
               {listing.name} - ₹{" "}
               {listing.offer
                 ? listing.discountPrice.toLocaleString("en-US")
-                : listing.regularPrice.toLocaleString("en-US")}
+                : listing.reqularPrice.toLocaleString("en-US")}
               {listing.type === "rent" && " / month"}
             </p>
             <p className="flex items-center mt-6 gap-2 text-slate-600  text-sm">
